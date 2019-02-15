c981e1b7e5b01e46b4f79acb62ba025e6b4b0211
GiboProgrammer <gift.malolo@baobabhealth.org>
2019-02-15 23:36:20 +0200

    duplicate error resolved

--- bin/healthdata_migration.rb
+++ bin/healthdata_migration.rb
@@ -127,11 +127,12 @@ def create_encounter(con,patient_id,location_id,date_created,orderer)
     date_created = date_created
     voided = 0
     provider = 92
-    encounter_id = order_counter = con.query("SELECT encounter_id AS total FROM encounter ORDER BY encounter_id desc limit 1").as_json[0]['total'].to_i +  1
+    #encounter_id = order_counter = con.query("SELECT encounter_id AS total FROM encounter ORDER BY encounter_id desc limit 1").as_json[0]['total'].to_i +  1
     uuid = get_uuid(con)
     encounter_type = con.query("SELECT encounter_type_id AS encout_type FROM encounter_type WHERE name ='LAB'").as_json[0]['encout_type']
-    con.query("INSERT INTO encounter (encounter_id,encounter_type,patient_id,provider_id,location_id,encounter_datetime,creator,date_created,voided,uuid) 
-                VALUES('#{encounter_id}','#{encounter_type}','#{patient_id}','#{provider}','#{location_id}','#{encounter_date}','#{creator}','#{date_created}','#{voided}','#{uuid}')")
+    con.query("INSERT INTO encounter (encounter_type,patient_id,provider_id,location_id,encounter_datetime,creator,date_created,voided,uuid) 
+                VALUES('#{encounter_type}','#{patient_id}','#{provider}','#{location_id}','#{encounter_date}','#{creator}','#{date_created}','#{voided}','#{uuid}')")
+    encounter_id  = con.query("SELECT encounter_id AS total FROM encounter ORDER BY encounter_id desc limit 1").as_json[0]['total'].to_i
     return encounter_id
 end
 
@@ -146,14 +147,15 @@ def escape_characters(value)
   return value
 end
 
-# creating observation for inserting order to openmrs
+# creating observation for inserting order to openmrs6fba15f8e769
 def create_observation(con,person_id,encounter_id,location_id,concept_id,datetime)
     uuid = get_uuid(con)
     obs_datetime = datetime
-    obs_id = obs_counter = con.query("SELECT obs_id AS total FROM obs ORDER BY encounter_id desc limit 1").as_json[0]['total'].to_i +  1
-    con.query("INSERT INTO obs (obs_id,person_id,concept_id,encounter_id,obs_datetime,location_id,creator,date_created,voided,uuid)
-        VALUES('#{obs_id}','#{person_id}','#{concept_id}','#{encounter_id}','#{obs_datetime}','#{location_id}','#{4}','#{obs_datetime}','#{0}','#{uuid}')
+    #obs_id = obs_counter = con.query("SELECT obs_id AS total FROM obs ORDER BY encounter_id desc limit 1").as_json[0]['total'].to_i +  1
+    con.query("INSERT INTO obs (person_id,concept_id,encounter_id,obs_datetime,location_id,creator,date_created,voided,uuid)
+        VALUES('#{person_id}','#{concept_id}','#{encounter_id}','#{obs_datetime}','#{location_id}','#{4}','#{obs_datetime}','#{0}','#{uuid}')
     ")
+    obs_id = con.query("SELECT obs_id AS total FROM obs ORDER BY encounter_id desc limit 1").as_json[0]['total'].to_i +  1
     return obs_id
 end
 
