0a5cb933acd7d91015393a3576d585b71f887223
Catchit <precious.bondwe@gmail.com>
2020-08-04 13:00:32 +0200

    Sanitised the querry to get data for migration. Added status mappings

--- bin/covid_data_migration.rb
+++ bin/covid_data_migration.rb
@@ -21,11 +21,24 @@ def load_defaults()
   @target_port = covid_conf['target']['port']
   @target_protocol = covid_conf['target']['protocol']
   @token = covid_conf['target']['token']
-  @user = covid_conf['target']['username']
-  @pass = covid_conf['target']['password']
+  @username = covid_conf['target']['username']
+  @password = covid_conf['target']['password']
+  @user = covid_conf['target']['other_user']
+  @pass = covid_conf['target']['other_password']
 
-  @logger = Logger.new("migration_log_#{Time.parse(DateTime.now.to_s)}.txt")
+  @logger_debug = Logger.new("migration_log_debug#{Time.parse(DateTime.now.to_s)}.txt")
+  @logger_transactional = Logger.new("migration_log_transactional#{Time.parse(DateTime.now.to_s)}.txt")
 
+  #mapping of statuses Covid19-Data => NLIMS 
+  @statuses = {
+              'Inputted' => 'started',
+              'Rejected' => 'test_rejected',
+              'In-Queue' => 'drawn',
+              'in-Process' => 'started',
+              'Tested' => 'completed',
+              'Dispatched' => 'verified',
+              'In-Transit' => 'completed'
+            }
 end
 
 def validate_token(m_token)
@@ -40,7 +53,7 @@ def validate_token(m_token)
   url = "#{@target_protocol}://#{@target_host}:#{@target_port}#{@target_prefix}check_token_validity"
 
   result = JSON.parse(RestClient.get(url,headers))
-
+  
   if (result["error"] == false)
     @token = m_token
     return @token
@@ -49,15 +62,15 @@ def validate_token(m_token)
     content_type: "text/plain",
     token: m_token
     }
-    url = "#{@target_protocol}://#{@target_host}:#{@target_port}#{@target_prefix}re_authenticate/#{@user}/#{@pass}"
+    url = "#{@target_protocol}://#{@target_host}:#{@target_port}#{@target_prefix}re_authenticate/#{@username}/#{@password}"
     res = JSON.parse(RestClient.get(url,headers))
 
     if (res["error"] == false)
-      @logger.info("Re-Authenticated the token")
+      @logger_debug.info("Re-Authenticated the token")
       @token = res["data"]["token"]
       return @token
     else
-      @logger.info("Re-Authenticated the token. message: #{res["message"]}")
+      @@logger_debug.info("Re-Authenticated the token. message: #{res["message"]}")
       exit
     end
   end
@@ -73,7 +86,6 @@ def send_json(json_obj, target_function)
  
   if (target_function == 'create_order')
     url = "#{@target_protocol}://#{@target_host}:#{@target_port}#{@target_prefix}create_order" 
-    
     res = JSON.parse(RestClient.post(url,json_obj,headers))
     return res
   elsif (target_function == 'update_result')
@@ -91,10 +103,19 @@ def send_json(json_obj, target_function)
 
 end
 
-def log_results()
+def log_results(rep)
   # logging results to file
-end
+  # Actions taking place on a record
+  # 1 => Create Order in NLIMS
+  # 2 => Update Tracking number in EID/VL
+  # 3 => Update Result in NLIMS
+  # 4 => Update status in NLIMS
+  # Structure of Report (LabID, Tracking NUmber, Result, Status, actions {})
 
+@logger_transactional.info("LabID: #{rep["labid"]}, TrackingNo: #{rep["trackingno"]}, Result: #{rep["result"]}, Status: #{rep["status"]}, CreateOrder: #{rep["createorder"]}, UpdateTrackingNo: #{rep["updatetrackingno"]}, UpdateResult: #{rep["updateresult"]}, UpdateStatus: #{rep["updatestatus"]}")
+
+end
+puts "Starting time =>>>>> #{Time.now}"
 
 load_defaults
 
@@ -122,8 +143,8 @@ covid_data = source_conn.query("SELECT
                                   ss.state AS test_status,
                                   cs.datespecimenreceivedatlab AS date_received, 
                                   cs.datespecimentsenttolab AS date_dispatched,
-                                  cs.result_interpretation as test_result,
-                                  cs.dateresultupdated as test_result_date,
+                                  rs.name as test_result,
+                                  cs.dateapproved as test_result_date,
                                   cs.trackingno as tracking_number
                                 FROM case_samples cs
                                   INNER JOIN case_patient cp
@@ -132,14 +153,24 @@ covid_data = source_conn.query("SELECT
                                     ON tc.id = cp.testCase
                                   INNER JOIN samplestatus ss
                                     ON cs.status = ss.ID
-                                LIMIT 2")
-#raise covid_data.first.to_yaml
+                                  LEFT JOIN results rs
+                                    on rs.id = cs.result")
+                               # WHERE cs.LabID='QECHCOV000193'")
+
 covid_data.each do |data_e|
 
-  @logger.info("working on this record  #{data_e["lab_id"].to_s}, #{data_e["tracking_number"].to_s}, #{data_e["test_result"].to_s} ")
-  puts "working on this record " + data_e["lab_id"].to_s #for checking purposes
-
-  if (data_e["tracking_no"].blank?) #check if the record has a tracking number
+  report = {'labid' => data_e["lab_id"], 
+            'trackingno' => data_e["tracking_number"], 
+            'result' => data_e["test_result"], 
+            'status' => data_e["test_status"], 
+            'createorder' => 'No', 
+            'updatetrackingno' => 'No',
+            'updateresult' => 'No', 
+            'updatestatus' => 'No'
+          }
+  @logger_debug.info("working on this record  #{data_e["lab_id"].to_s}, #{data_e["tracking_number"].to_s}, #{data_e["test_result"].to_s} ")
+ 
+  if (data_e["tracking_number"].blank?) #check if the record has a tracking number
     data = {
       "district" => data_e["district"],
       "health_facility_name" => data_e["health_facility_name"],
@@ -169,15 +200,16 @@ covid_data.each do |data_e|
     dataJSON = JSON.generate(data)
     puts "sending orderdata to send_json"
     result = send_json(dataJSON, "create_order")
-    @logger.info("#{result.to_yaml}")
-    
+    puts "Created order"
     if (result["error"] == false)
       
-      @logger.info("Just added a new order for  #{data_e["lab_id"].to_s}, with #{ result['message']} #{result['data']}")
+      @logger_debug.info("Just added a new order for  #{data_e["lab_id"].to_s}, with #{ result['message']} #{result['data']}")
       puts "added new order #{ result['message']} #{result['data']}"
-
+      
       tracking_number = result["data"]["tracking_number"]
-      #test_result = data_e["test_result"]
+      report["trackingno"] = tracking_number
+      report["createorder"] = 'Yes'
+     
       lab_id = data_e["lab_id"]
 
       #update tracking number
@@ -185,12 +217,13 @@ covid_data.each do |data_e|
                         SET trackingno = '#{tracking_number}'
                         WHERE LabID = '#{lab_id}'")
                        
-      
-      @logger.info("Just updated the tracking number for  #{data_e["lab_id"].to_s} with this value #{tracking_number}")
+      report["updatetrackingno"] = 'Yes'
+
+      @logger_debug.info("Just updated the tracking number for  #{data_e["lab_id"].to_s} with this value #{tracking_number}")
       puts "Just updated the tracking number for " + data_e["lab_id"].to_s
 
       if (!data_e["test_result"].blank?)
-        @logger.info("This #{data_e["lab_id"].to_s} has a test result ")
+        @logger_debug.info("This #{data_e["lab_id"].to_s} has a test result ")
         result_data = {
           "tracking_number" => tracking_number,
           "test_status" => 'verified',
@@ -199,59 +232,61 @@ covid_data.each do |data_e|
           "who_updated" => {
                               'id':'31',
                               'phone_number':'',
-                              'first_name':'Bernard',
-                              'last_name':'Kanfosi'
-                           },
+                              'first_name':"#{@user}",
+                              'last_name':"#{@pass}" 
+                          },                         
           "results" => {
                         "Covid19":"#{data_e["test_result"]}"   
                        }  
         }
 
         dataJSON = JSON.generate(result_data)
-        @logger.info("sending reult data to send_json -- Update Result    #{data_e["lab_id"].to_s}" )
+        @logger_debug.info("sending reult data to send_json -- Update Result    #{data_e["lab_id"].to_s}" )
         result_update = send_json(dataJSON, "update_result")
-        @logger.info("#{result_update.to_yaml}")
-
+        
         if (result_update["error"] == false)
-          @logger.info("Just updated the result in NLIMS for #{data_e["lab_id"].to_s}")
+          @logger_debug.info("Just updated the result in NLIMS for #{data_e["lab_id"].to_s}")
             puts "Just updated the result for " + data_e["lab_id"].to_s
+            report["updateresult"] = 'Yes'
         else
-          @logger.info("Failed to update the result in NLIMS for #{data_e["lab_id"].to_s}")
+          @logger_debug.info("Failed to update the result in NLIMS for #{data_e["lab_id"].to_s}")
             puts "Failed to update the result for " + data_e["lab_id"].to_s
+            report["updateresult"] = 'Failed'
         end
 
       else
-        @logger.info("attempting updating result for  #{data_e["lab_id"].to_s}, with message #{result['message']} , #{result['data']}")
+        @logger_debug.info("attempting updating status for  #{data_e["lab_id"].to_s}")
         status_data = {
           "tracking_number" => tracking_number,
-          "test_status" => data_e["test_status"],
+          "test_status" => @statuses["#{data_e["test_status"]}"],
           "test_name" => "Covid19",
           "result_date" => data_e["test_result_date"],
           "who_updated" => {
                               'id':'31',
                               'phone_number':'',
-                              'first_name':'Bernard',
-                              'last_name':'Kanfosi'
+                              'first_name':"#{@user}",
+                              'last_name':"#{@pass}"
                            }
-        }
+                       }
 
         dataJSON = JSON.generate(status_data)
-        @logger.info("sending reult data to send_json -- Update Result    #{data_e["lab_id"].to_s}" )
+        @logger_debug.info("sending reult data to send_json -- Update Result    #{data_e["lab_id"].to_s}" )
         status_update = send_json(dataJSON, "update_status")
-        @logger.info("#{status_update.to_yaml}")
-
+        
         if (status_update["error"] == false)
-          @logger.info("Just updated the status for  #{data_e["lab_id"].to_s}")
+          @logger_debug.info("Just updated the status for  #{data_e["lab_id"].to_s}")
           puts "Just updated the status for " + data_e["lab_id"].to_s
+          report["updatestatus"] = 'Yes'
         else
-          @logger.info("Failed to update the status for  #{data_e["lab_id"].to_s}")
+          @logger_debug.info("Failed to update the status for  #{data_e["lab_id"].to_s}")
           puts "Failed to update the status for " + data_e["lab_id"].to_s
+          report["updatestatus"] = 'Failed'
         end
       end
     end
   else
     if (!data_e["test_result"].blank?)
-      @logger.info("This #{data_e["lab_id"].to_s} has a test result ")
+      @logger_debug.info("This #{data_e["lab_id"].to_s} has a test result ")
       res_data = {
           "tracking_number" => data_e["tracking_number"],
           "test_status" => 'verified',
@@ -260,8 +295,8 @@ covid_data.each do |data_e|
           "who_updated" => {
                                 'id':'31',
                                 'phone_number':'',
-                                'first_name':'Bernard',
-                                'last_name':'Kanfosi'
+                                'first_name':"#{@user}",
+                                'last_name':"#{@pass}"
                             },
           "results" => {
                         "Covid19":"#{data_e["test_result"]}"   
@@ -269,51 +304,54 @@ covid_data.each do |data_e|
         }
 
       dataJSON = JSON.generate(res_data)
-      @logger.info("sending reult data to send_json -- Update Result    #{data_e["lab_id"].to_s}" )
+      @logger_debug.info("sending reult data to send_json -- Update Result    #{data_e["lab_id"].to_s}" )
       res_update = send_json(dataJSON, "update_result")
-      @logger.info("#{res_update.to_yaml}")
-
+      
       if (res_update["error"] == false)
-        @logger.info("Just updated the result for  #{data_e["lab_id"].to_s}")
+        @logger_debug.info("Just updated the result for  #{data_e["lab_id"].to_s}")
         puts "Just updated the result for " + data_e["lab_id"].to_s
+        report["updateresult"] = 'Yes'
       else
-        @logger.info("Failed to update the result for  #{data_e["lab_id"].to_s}")
+        @logger_debug.info("Failed to update the result for  #{data_e["lab_id"].to_s}")
           puts "Failed to update the result for " + data_e["lab_id"].to_s
+          report["updateresult"] = 'Failed'
       end
     else
-      @logger.info("attempting updating result for  #{data_e["lab_id"].to_s}, with message #{result['message']} , #{result['data']}")
+      @logger_debug.info("attempting to update status for  #{data_e["lab_id"].to_s}")
 
       stat_data = {
           "tracking_number" => data_e["tracking_number"],
-          "test_status" => data_e["test_status"],
+          "test_status" => @statuses["#{data_e["test_status"]}"],
           "test_name" => "Covid19",
           "result_date" => data_e["result_date"],
           "who_updated" => {
                                 'id':'31',
                                 'phone_number':'',
-                                'first_name':'Bernard',
-                                'last_name':'Kanfosi'
+                                'first_name':"#{@user}",
+                                'last_name':"#{@pass}"
                             }
         }
 
       dataJSON = JSON.generate(stat_data)
 
-      @logger.info("sending reult data to send_json -- Update Result    #{data_e["lab_id"].to_s}" )
+      @logger_debug.info("sending reult data to send_json -- Update Result    #{data_e["lab_id"].to_s}" )
       stat_update = send_json(dataJSON, "update_status")
-      @logger.info("#{status_update.to_yaml}")
-
+     
       if (stat_update["error"] == false)
-        @logger.info("Just updated the status for  #{data_e["lab_id"].to_s}")
+        @logger_debug.info("Just updated the status for  #{data_e["lab_id"].to_s}")
         puts "Just updated the status for " + data_e["lab_id"].to_s
+        report["updatestatus"] = 'Yes'
       else
-        @logger.info("Failed to update the status for  #{data_e["lab_id"].to_s}")
+        @logger_debug.info("Failed to update the status for  #{data_e["lab_id"].to_s}")
         puts "Failed to update the status for " + data_e["lab_id"].to_s
+        report["updatestatus"] = 'Failed'
       end
     end
   end 
- 
+ log_results(report)
 end
 
 #Print results of the migration
+puts "Ending time =>>>>> #{Time.now}"
 
 puts "Done!!"
